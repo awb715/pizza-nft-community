388421229fd64e58324fe22d211e767717ae8529
awb715 <aaronbellwebdev@gmail.com>
2024-02-28 21:37:24 -0700

    change to baseSepolia

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,36 +1,40 @@
 import "@/styles/globals.css";
 import { WagmiProvider, createConfig, http } from "wagmi";
-import { mainnet } from "wagmi/chains";
+import { arbitrumSepolia, baseSepolia, mainnet, polygon, polygonMumbai } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+//way to fetch data
 import { ConnectKitProvider, getDefaultConfig } from "connectkit";
+//connects walletr
 import { ThemeProvider } from "@/components/themes";
 
 import type { AppProps } from "next/app";
 const config = createConfig(
   getDefaultConfig({
     // Your dApps chains
-    chains: [mainnet],
+    chains: [baseSepolia],
     transports: {
       // RPC URL for each chain
-      [mainnet.id]: http(
-        `https://eth-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_MAINNET}`,
-      ),
+      [baseSepolia.id]: http(
+        `https://base-sepolia.blockpi.network/v1/rpc/public`,
+      )
     },
+
 
     // Required API Keys
     walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || '',
 
     // Required App Info
-    appName: "Crypto Portfolio",
+    appName: "CryptoSlices",
 
     // Optional App Info
-    appDescription: "A great app for managing your portfolio",
-    appUrl: "https://family.co", // your app's url
-    appIcon: "https://family.co/logo.png", // your app's icon, no bigger than 1024x1024px (max. 1MB)
+    appDescription: "Share your pizza cost in ETH or Satoshi and earn an NFT. Then, discover what others paid for a slice!",
+    appUrl: "🍕", // your app's url
+    appIcon: "🍕", // your app's icon, no bigger than 1024x1024px (max. 1MB)
   }),
 );
 export default function App({ Component, pageProps }: AppProps) {
 
+  
 const queryClient = new QueryClient();
   return (
     <WagmiProvider config={config}>
