8791aa59d4f1f238f7463c0adac67194cd05e361
awb715 <aaronbellwebdev@gmail.com>
2024-03-01 14:06:02 -0700

    contract address

--- src/data/project-summary.ts
+++ src/data/project-summary.ts
@@ -1,7 +1,7 @@
 const projectInfo = {
   name: "CryptoSlices",
   bio: "How much ETH / Satoshi did a slice of pizza cost you?",
-  address: "0xE66400BBD0bD5c300aA129C64B929A0872dEDE90",
+  nftAddress: "0xb5954589190E143767120323970e3440A4454918",
   links: [
     {
       name: "Cheese",
@@ -13,14 +13,14 @@ const projectInfo = {
     },
     {
       name: "Pepperoni",
-      url: "IPFS-Pepperoni"
+      url: "https://gray-odd-lynx-187.mypinata.cloud/ipfs/QmQdEPS2MCKAra8E4nwfs37Kh88wM1cStxUFC7WZm97eyV"
     }]
 }
 
 const linkMap = {};
 
-projectInfo.links.forEach((link) => {
-  linkMap[link.name] = link;
-});
+// projectInfo.links.forEach((link) => {
+//   linkMap[link.name] = link;
+// });
 
 export {projectInfo,linkMap}
