ccc156bd6ca57564c3dfc35d4c72ac0feb9f6664
awb715 <aaronbellwebdev@gmail.com>
2024-03-02 03:27:58 -0700

    gate other slices

--- src/components/NFTLoaders/index.tsx
+++ src/components/NFTLoaders/index.tsx
@@ -1,14 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import { fetchNFTDataFromAPI } from '../../lib/hooks/useNFTCollectibles';// Adjust the path
 import { SimpleNFTComponent } from './nft-card';
+import { useFetchNFTData } from '@/lib/hooks/nfttest';
 
+interface NFTGalleryProps {
+	contractAddress: string;
+	userAddress?: string; // Allow userAddress to be undefined
+  }
+  
 // Adjust the path
 // ... other imports
-
-const NFTGallery: React.FC<{ contractAddress: string }> = ({ contractAddress }) => {
+const NFTGallery: React.FC<NFTGalleryProps> = ({ contractAddress, userAddress }) => {
 	const [nftData, setNFTData] = useState<any[]>([]); // Change the type if needed
 	const [loading, setLoading] = useState<boolean>(false);
 	const [error, setError] = useState<string | null>(null);
+	const { value, data, error2 } = useFetchNFTData(userAddress);
+	console.log(value, ' -length')
   
 	useEffect(() => {
 	  const fetchData = async () => {
@@ -37,6 +44,10 @@ const NFTGallery: React.FC<{ contractAddress: string }> = ({ contractAddress }) 
 	if (error) {
 	  return <div>Error: {error}</div>;
 	}
+	
+	if (value === 0) {
+		return <div>You need to share your pizza price to see other people's slices.</div>;
+	  }
   
 	return (
 	  <div>
@@ -48,7 +59,7 @@ const NFTGallery: React.FC<{ contractAddress: string }> = ({ contractAddress }) 
 		  ))}
 		</div>
 	  </div>
-	);
+	);//
   };
   
   export { NFTGallery };
